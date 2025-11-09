ee06832ee854856580eff6305e95037f49c236db
shabanelmoogy <shaban.cec@gmail.com>
2025-11-09 15:40:11 +0200

    update scrollbar

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9fnmh4cd20g"
+    "revision": "0.36nkko3ikeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
